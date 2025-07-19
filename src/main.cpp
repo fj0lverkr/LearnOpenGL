18ce9480f5be10236e38ee4bd1b2062a76ae24ff
Nils Nahooy <nils.nahooy@live.com>
2025-07-19 15:28:51 +0200

    Handle porgram exit

--- src/main.cpp
+++ src/main.cpp
@@ -4,48 +4,66 @@
 
 using namespace std;
 
-void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+constexpr int GLFW_MAJOR_VERSION = 3;
+constexpr int GLFW_MINOR_VERSION = 3;
+
+void framebufferSizeCallback(GLFWwindow *window, int width, int height)
 {
 	glViewport(0, 0, width, height);
+}
+
+void processWindowInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
+int exit_clean(int const &code, string const &reason = "")
+{
+	if (reason != "")
+	{
+		cout << reason << endl;
+	}
+
+	glfwTerminate();
+	return code;
 }
 
 int main()
 {
 	if (!glfwInit())
 	{
-		cout << "Failed to initialize GLFW, exiting..." << endl;
-		return -1;
+		exit_clean(-1, "Failed to initialize GLFW, exiting...");
 	}
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, GLFW_MAJOR_VERSION);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, GLFW_MINOR_VERSION);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	// glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); //Mac OSX only
 
 	GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
 	if (window == NULL)
 	{
-		cout << "Failed to create GLFW window, exiting..." << endl;
-		glfwTerminate();
-		return -1;
+		exit_clean(-1, "Failed to create GLFW window, exiting...");
 	}
 	glfwMakeContextCurrent(window);
 
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
-		cout << "Failed to initialize GLAD, exiting..." << endl;
-		return -1;
+		exit_clean(-1, "Failed to initialize GLAD, exiting...");
 	}
 
 	glViewport(0, 0, 800, 600);
 
-	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
 	while (!glfwWindowShouldClose(window))
 	{
+		processWindowInput(window);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
 
-	return 0;
+	exit_clean(0);
 }
